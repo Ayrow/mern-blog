9aa6b3b249a19e6f8ddc91c0ce59f62c2ca664f9
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2022-12-29 15:10:26 +0100

    setup some routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import {
+  LandingPage,
+  ErrorPage,
+  PostsPage,
+  LoginPage,
+  ProtectedRoute,
+} from './pages/index';
+
 function App() {
-  return <div>This will be a blog</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='posts' element={<PostsPage />} />
+
+        <Route path='/dashboard/' element={<ProtectedRoute></ProtectedRoute>} />
+
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import LandingPage from './LandingPage';
+import ErrorPage from './ErrorPage';
+import LoginPage from './LoginPage';
+import PostsPage from './PostsPage';
+import ProtectedRoute from './ProtectedRoute';
+
+export { LandingPage, ErrorPage, LoginPage, PostsPage, ProtectedRoute };
