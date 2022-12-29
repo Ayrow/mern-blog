89f06f59132244d7c23e532593fdffaa0c70b30b
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2022-12-29 14:39:30 +0100

    updated readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div></div>;
+  return <div>This will be a blog</div>;
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -11,16 +11,16 @@ app.get('/', (req, res) => {
   res.send('Welcome');
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
-      console.log(`server is listening on port ${port}`);
+      console.log(`server is running on port ${port}`);
     });
   } catch (error) {
-    console.log('error', error);
+    console.log(error);
   }
 };
 
