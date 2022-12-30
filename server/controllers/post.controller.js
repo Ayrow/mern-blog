f39de84c32b0b3886b2f0f9103f6cb4d289d5ec0
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2022-12-30 15:56:32 +0100

    setup posts controller

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,42 @@
+const addPost = async (req, res) => {
+  res.status(200).json({ msg: 'addPost' });
+};
+
+const getAllPosts = async (req, res) => {
+  res.status(200).json({ msg: 'getAllPosts' });
+};
+
+const commentPost = async (req, res) => {
+  res.status(200).json({ msg: 'comment Post' });
+};
+
+const getSinglePost = async (req, res) => {
+  res.status(200).json({ msg: 'get Single Post' });
+};
+
+const deletePost = async (req, res) => {
+  res.status(200).json({ msg: 'delete Post' });
+};
+
+const savePost = async (req, res) => {
+  res.status(200).json({ msg: 'save post' });
+};
+
+const getAllSavedPosts = async (req, res) => {
+  res.status(200).json({ msg: 'get all saved posts' });
+};
+
+const deleteSavedPost = async (req, res) => {
+  res.status(200).json({ msg: 'delete saved post' });
+};
+
+export {
+  getAllPosts,
+  addPost,
+  commentPost,
+  getSinglePost,
+  deletePost,
+  savePost,
+  getAllSavedPosts,
+  deleteSavedPost,
+};
