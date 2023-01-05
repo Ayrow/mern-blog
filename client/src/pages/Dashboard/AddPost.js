1b33e1b1d1fc787eb7839bf349b9cc8315f4a463
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2023-01-05 11:02:11 +0100

    setup add post page with rich text editor

--- client/src/pages/Dashboard/AddPost.js
+++ client/src/pages/Dashboard/AddPost.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const AddPost = () => {
   return (
-    <div>
-      <div>
-        <input type='text' />
+    <div className='h-full mx-auto container mt-5 md:w-3/4 shadow-md border-t-2 border-indigo-400 rounded-t pb-10'>
+      <div className='bg-white space-y-6 '>
+        <div className='flex flex-col items-center mt-5'>
+          <input
+            type='text'
+            className=' focus:outline-none focus:text-gray-600 ml-4'
+            placeholder='Post Title'
+          />
+          <ReactQuill
+            theme='snow'
+            name='postText'
+            // value={travelDetail}
+            className=' text-black w-full px-5 py-5'
+            placeholder='Write your post'
+          />
+        </div>
+        <div className='flex justify-end pr-10 pt-5'>
+          <button className=' bg-green-700 text-white px-3 py-1 rounded-lg text-lg uppercase'>
+            Save
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Dashboard/ProfileSettings.js
+++ client/src/pages/Dashboard/ProfileSettings.js
@@ -4,32 +4,33 @@ const ProfileSettings = () => {
   const { user } = useUserContext();
 
   return (
-    <div class='mx-auto container mt-5 md:w-3/4 shadow-md'>
-      <div class='bg-gray-100 p-4 border-t-2 bg-opacity-5 border-indigo-400 rounded-t'>
-        <div class='max-w-sm mx-auto md:w-full md:mx-0'>
-          <div class='inline-flex items-center space-x-4'>
+    <div className='mx-auto container mt-5 md:w-3/4 shadow-md'>
+      <div className='bg-gray-100 p-4 bg-opacity-5 border-t-2 border-indigo-400 rounded-t'>
+        <div className='max-w-sm mx-auto md:w-full md:mx-0'>
+          <div className='inline-flex items-center space-x-4'>
             <img
-              class='w-10 h-10 object-cover rounded-full'
+              className='w-10 h-10 object-cover rounded-full'
               alt='User avatar'
               src='https://avatars3.githubusercontent.com/u/72724639?s=400&u=964a4803693899ad66a9229db55953a3dbaad5c6&v=4'
             />
 
-            <h1 class='text-gray-600'>Hello {user.username}</h1>
+            <h1 className='text-gray-600'>Hello {user.username}</h1>
           </div>
         </div>
       </div>
-      <div class='bg-white space-y-6'>
+
+      <div className='bg-white space-y-6'>
         <hr />
-        <div class='md:inline-flex  space-y-4 md:space-y-0  w-full p-4 text-gray-500 items-center'>
-          <h2 class='md:w-1/3 mx-auto max-w-sm'>Personal info</h2>
-          <div class='md:w-2/3 mx-auto max-w-sm space-y-5'>
+        <div className='md:inline-flex  space-y-4 md:space-y-0  w-full p-4 text-gray-500 items-center'>
+          <h2 className='md:w-1/3 mx-auto max-w-sm'>Personal info</h2>
+          <div className='md:w-2/3 mx-auto max-w-sm space-y-5'>
             <div>
-              <label class='text-sm text-gray-400'>Full name</label>
-              <div class='w-full inline-flex border'>
-                <div class='w-1/12 pt-2 bg-gray-100'>
+              <label className='text-sm text-gray-400'>Username</label>
+              <div className='w-full inline-flex border'>
+                <div className='w-1/12 pt-2 bg-gray-100'>
                   <svg
                     fill='none'
-                    class='w-6 text-gray-400 mx-auto'
+                    className='w-6 text-gray-400 mx-auto'
                     viewBox='0 0 24 24'
                     stroke='currentColor'>
                     <path
@@ -42,7 +43,7 @@ const ProfileSettings = () => {
                 </div>
                 <input
                   type='text'
-                  class='w-11/12 focus:outline-none focus:text-gray-600 p-2'
+                  className='w-11/12 focus:outline-none focus:text-gray-600 p-2'
                   placeholder='Charly Olivas'
                 />
               </div>
@@ -51,15 +52,15 @@ const ProfileSettings = () => {
         </div>
 
         <hr />
-        <div class='md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center'>
-          <h2 class='md:w-4/12 max-w-sm mx-auto'>Change password</h2>
+        <div className='md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center'>
+          <h2 className='md:w-4/12 max-w-sm mx-auto'>Change password</h2>
 
-          <div class='md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2'>
-            <div class='w-full inline-flex border-b'>
-              <div class='w-1/12 pt-2'>
+          <div className='md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2'>
+            <div className='w-full inline-flex border-b'>
+              <div className='w-1/12 pt-2'>
                 <svg
                   fill='none'
-                  class='w-6 text-gray-400 mx-auto'
+                  className='w-6 text-gray-400 mx-auto'
                   viewBox='0 0 24 24'
                   stroke='currentColor'>
                   <path
@@ -72,17 +73,17 @@ const ProfileSettings = () => {
               </div>
               <input
                 type='password'
-                class='w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4'
+                className='w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4'
                 placeholder='New'
               />
             </div>
           </div>
 
-          <div class='md:w-3/12 text-center md:pl-6'>
-            <button class='text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right'>
+          <div className='md:w-3/12 text-center md:pl-6'>
+            <button className='text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right'>
               <svg
                 fill='none'
-                class='w-4 text-white mr-2'
+                className='w-4 text-white mr-2'
                 viewBox='0 0 24 24'
                 stroke='currentColor'>
                 <path
@@ -98,11 +99,11 @@ const ProfileSettings = () => {
         </div>
 
         <hr />
-        <div class='w-full p-4 text-right text-red-500'>
-          <button class='inline-flex items-center focus:outline-none mr-4'>
+        <div className='w-full p-4 text-right text-red-500'>
+          <button className='inline-flex items-center focus:outline-none mr-4'>
             <svg
               fill='none'
-              class='w-4 mr-2'
+              className='w-4 mr-2'
               viewBox='0 0 24 24'
               stroke='currentColor'>
               <path
