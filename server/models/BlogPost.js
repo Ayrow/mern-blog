f83dac1fb734a95306ec00e0643c1fc404c1782e
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2023-01-07 12:22:34 +0100

    added short description to blogpost model

--- server/models/BlogPost.js
+++ server/models/BlogPost.js
@@ -6,6 +6,12 @@ const BlogPostSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Please provide a title'],
     },
+    shortDescription: {
+      type: String,
+      minlength: [20, 'Short description must be more than 20 characters'],
+      maxlength: [150, 'Short description must be less than 150 characters'],
+      default: 'No short description has been provided.',
+    },
     postText: {
       type: String,
       required: [true, 'Please provide text'],
