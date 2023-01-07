209d8e5a25a2141f90e25325ec43dc886455d336
Aymeric Pilaert <aymeric.pilaert@hotmail.fr>
2023-01-07 12:15:56 +0100

    setup basic cards to display posts on all posts page

--- client/src/pages/PostsPage.js
+++ client/src/pages/PostsPage.js
@@ -2,12 +2,37 @@ import { useEffect } from 'react';
 import { usePostsContext } from '../context/posts/posts.context';
 
 const PostsPage = () => {
-  const { getAllPosts } = usePostsContext();
+  const { getAllPosts, posts } = usePostsContext();
 
   useEffect(() => {
     getAllPosts();
   }, []);
-  return <div>PostsPage</div>;
+
+  return (
+    <div>
+      <div>
+        <h2 className='text-2xl font-bold text-center mt-5  '>
+          Discover all posts!
+        </h2>
+      </div>
+      <div className='grid grid-cols-1 px-5 py-10'>
+        {posts.map((post) => {
+          const { title, postText } = post;
+          return (
+            <div className='border p-5 rounded-lg shadow-md border-gray-400 bg-slate-50'>
+              <h3 className='text-center text-xl font-semibold'>{title}</h3>
+              <p className='pt-3'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Laborum nemo, odit consequatur aliquam iure et laudantium aut
+                sed doloremque tenetur, praesentium modi assumenda. Aspernatur
+                aliquid iure ad. Voluptatem, cupiditate necessitatibus!
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default PostsPage;
