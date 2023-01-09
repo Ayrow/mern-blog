bcc1b4e6f8331f30254d2c2274ef2f6bc424abc0
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-09 11:08:41 +0100

    added users route to fetch all users

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,10 +4,12 @@ import {
   loginUser,
   deleteUser,
   updateUser,
+  getAllUsers,
 } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
+router.route('/users').get(getAllUsers);
 router.route('/registerUser').post(registerUser);
 router.route('/loginUser').post(loginUser);
 router.route('/updateUser').patch(updateUser);
