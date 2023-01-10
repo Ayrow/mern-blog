26e35d86e2ad3fdaf732fb28f06c1194e13aba65
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-10 11:26:15 +0100

    refactored dashboard menu admin

--- client/src/components/DashboardMenuAdmin.js
+++ client/src/components/DashboardMenuAdmin.js
@@ -1,45 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import { adminLinks } from '../utils/menuLinks';
 
 const DashboardMenuAdmin = () => {
   return (
     <div className=' relative w-1/6 bg-gray-800 h-full text-white px-5 pt-10'>
       <div className='fixed flex flex-col items-center gap-10'>
-        <NavLink
-          to={'/dashboard/add-post'}
-          className={({ isActive }) =>
-            isActive
-              ? ' hover:bg-slate-600 bg-slate-700 p-3'
-              : ' hover:bg-slate-600 p-3'
-          }>
-          Add New Post
-        </NavLink>
-        <NavLink
-          to={'/dashboard/all-posts'}
-          className={({ isActive }) =>
-            isActive
-              ? ' hover:bg-slate-600 bg-slate-700 p-3'
-              : ' hover:bg-slate-600 p-3'
-          }>
-          All Posts
-        </NavLink>
-        <NavLink
-          to={'/dashboard/manage-users'}
-          className={({ isActive }) =>
-            isActive
-              ? ' hover:bg-slate-600 bg-slate-700 p-3'
-              : ' hover:bg-slate-600 p-3'
-          }>
-          Manage users
-        </NavLink>
-        <NavLink
-          to='/dashboard/profile-settings'
-          className={({ isActive }) =>
-            isActive
-              ? ' hover:bg-slate-600 bg-slate-700 p-3'
-              : ' hover:bg-slate-600 p-3'
-          }>
-          Profile Settings
-        </NavLink>
+        {adminLinks.map((link) => {
+          const { id, text, path } = link;
+          return (
+            <NavLink
+              to={path}
+              key={id}
+              className={({ isActive }) =>
+                isActive
+                  ? ' hover:bg-slate-600 bg-slate-700 p-3'
+                  : ' hover:bg-slate-600 p-3'
+              }>
+              {text}
+            </NavLink>
+          );
+        })}
       </div>
     </div>
   );
---
+++ client/src/utils/menuLinks.js
@@ -0,0 +1,22 @@
+export const adminLinks = [
+  {
+    id: 1,
+    text: 'Add New Post',
+    path: '/dashboard/add-post',
+  },
+  {
+    id: 2,
+    text: 'All Posts',
+    path: '/dashboard/all-posts',
+  },
+  {
+    id: 3,
+    text: 'Manage users',
+    path: '/dashboard/manage-users',
+  },
+  {
+    id: 4,
+    text: 'Profile Settings',
+    path: '/dashboard/profile-settings',
+  },
+];
