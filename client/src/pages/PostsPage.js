fa7cffbf0f0a55035f5d1ab3bcb4387144e729cc
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-13 12:13:58 +0100

    few tweaks for posts page design

--- client/src/pages/PostsPage.js
+++ client/src/pages/PostsPage.js
@@ -16,18 +16,18 @@ const PostsPage = () => {
           Discover all posts!
         </h2>
       </div>
-      <div className='grid grid-cols-3 px-5 py-10'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-5 py-10 gap-2'>
         {posts.map((post) => {
           const { title, shortDescription, _id } = post;
           return (
             <div
               key={_id}
-              className='border p-5 rounded-lg shadow-md border-gray-400 bg-slate-50'>
-              <h3 className='text-center text-xl font-semibold'>{title}</h3>
+              className='border p-5 rounded-lg shadow-md border-gray-400 bg-slate-50 flex flex-col items-center'>
+              <h3 className='text-xl font-semibold'>{title}</h3>
               <p className='pt-3'>{shortDescription}</p>
               <Link
                 to={`/posts/${_id}`}
-                className='flex justify-center mt-5 border p-2 bg-blue-900 text-white hover:bg-blue-800'>
+                className='flex justify-center mt-5 border p-2 bg-blue-900 text-white hover:bg-blue-800 w-1/3 rounded-md'>
                 Read more
               </Link>
             </div>
