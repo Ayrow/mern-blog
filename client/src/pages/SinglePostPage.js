b702843fc5a9462ed848674f08a9b499af56db9f
Aymeric <aymeric.pilaert@hotmail.fr>
2023-02-08 10:57:11 +0100

    setup design for comment section in post page

--- client/src/pages/SinglePostPage.js
+++ client/src/pages/SinglePostPage.js
@@ -13,22 +13,39 @@ const SinglePostPage = () => {
   }, []);
 
   return (
-    <div className=' bg-gray-200 m-10 p-10 rounded-lg relative'>
-      <button
-        type='button'
-        className=' bg-red-500 hover:bg-red-300 text-white px-3 py-1 rounded-lg float-right'>
-        Save
-      </button>
-      <div>
-        <h2 className='text-center text-2xl'>{singlePost.title}</h2>
+    <div className=''>
+      <div className=' bg-gray-200 m-10 p-10 rounded-lg relative'>
+        <button
+          type='button'
+          className=' bg-red-500 hover:bg-red-300 text-white px-3 py-1 rounded-lg float-right'>
+          Save
+        </button>
+        <div>
+          <h2 className='text-center text-2xl'>{singlePost.title}</h2>
+        </div>
+
+        <ReactQuill
+          value={singlePost.postText}
+          readOnly={true}
+          theme={'bubble'}
+          className='mt-5'
+        />
       </div>
 
-      <ReactQuill
-        value={singlePost.postText}
-        readOnly={true}
-        theme={'bubble'}
-        className=' mt-5'
-      />
+      <div className=' bg-slate-800 text-white p-10 flex flex-col gap-12'>
+        <h3 className='text-center text-xl'>Comment Section</h3>
+        <div>
+          <button
+            type='button'
+            className='border px-2 py-1 rounded-lg hover:bg-slate-500'>
+            Add comment
+          </button>
+        </div>
+
+        <div>
+          <p>All comments</p>
+        </div>
+      </div>
     </div>
   );
 };
