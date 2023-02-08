12bac38eebff44627afa9ebadad327c0e1e54dd3
Aymeric <aymeric.pilaert@hotmail.fr>
2023-02-08 10:48:07 +0100

    added save button on post page

--- client/src/pages/SinglePostPage.js
+++ client/src/pages/SinglePostPage.js
@@ -13,7 +13,12 @@ const SinglePostPage = () => {
   }, []);
 
   return (
-    <div className=' bg-gray-200 m-10 p-10 rounded-lg'>
+    <div className=' bg-gray-200 m-10 p-10 rounded-lg relative'>
+      <button
+        type='button'
+        className=' bg-red-500 hover:bg-red-300 text-white px-3 py-1 rounded-lg float-right'>
+        Save
+      </button>
       <div>
         <h2 className='text-center text-2xl'>{singlePost.title}</h2>
       </div>
