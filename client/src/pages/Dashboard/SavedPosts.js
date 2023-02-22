588e480af73ba23adedd9f12dc0259f783c1a1d3
Aymeric <aymeric.pilaert@hotmail.fr>
2023-02-22 15:25:47 +0100

    setup design for displaying all saved posts

--- client/src/pages/Dashboard/SavedPosts.js
+++ client/src/pages/Dashboard/SavedPosts.js
@@ -1,7 +1,29 @@
-import React from 'react';
-
 const SavedPosts = () => {
-  return <div>SavedPosts</div>;
+  return (
+    <div className='mx-auto container mt-5 md:w-3/4 shadow-md border-t-2 border-indigo-400 rounded-t p-10'>
+      <div>Filter and Sort Container</div>
+      <div className='mt-10 grid grid-cols-2 uppercase font-bold'>
+        <p className='text-center'>Post Title</p>
+        <p className='text-center'>Actions</p>
+      </div>
+      <div className='flex flex-col gap-5'>
+        {posts.map((item) => {
+          const { title, _id, shortDescription, postText } = item;
+          return (
+            <DashboardSingleItem
+              key={_id}
+              id={_id}
+              name={title}
+              isPost={true}
+              deleteItem={deletePost}
+              shortDescription={shortDescription}
+              body={postText}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default SavedPosts;
