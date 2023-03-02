f4cea4f20e479ac5c70a2307577e08c9db2f0c2b
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-02 09:11:04 +0100

    setup sorting for getAllComments in comments controller

--- server/controllers/comments.controller.js
+++ server/controllers/comments.controller.js
@@ -46,9 +46,36 @@ const deleteComment = async (req, res) => {
 };
 
 const getAllComments = async (req, res) => {
-  const allComments = await Comments.find();
+  const { sort } = req.query;
 
-  res.status(200).json(allComments);
+  const queryObject = {};
+
+  let result = Comments.find(queryObject);
+
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+
+  if (sort === 'a-z') {
+    result = result.sort('title');
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const allComments = await result;
+
+  const totalComments = await Comments.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalComments / limit);
+
+  res.status(200).json({ allComments, numOfPages, totalComments });
 };
 
 const getUserComments = async (req, res) => {
